782a3573031ee4233e18e1910c3cc16d69e203bb
Joseph Marsden <josephmarsden@towerdevs.xyz>
2017-03-19 07:17:24 +0000

    Fixed a really weird bug.

--- app/Http/Controllers/Configuration/OrganizationConfig.php
+++ app/Http/Controllers/Configuration/OrganizationConfig.php
@@ -15,15 +15,17 @@ class OrganizationConfig extends Controller implements Configuration {
 
       // Organization configuration
       "OrganizationName" => "CoreNIC", // Your organization's name.
-      "OrganizationKey" => "c86c83d2102d148edf2061809eadee9e605f2fe01a0afbf9980c81a06509", // Your organization's key.
+      //"OrganizationKey" => "c86c83d2102d148edf2061809eadee9e605f2fe01a0afbf9980c81a06509", // Your organization's key.
+      "OrganizationKey" => "91aefd4f3bee695e2939836cccbd8519e5d0dbe4a52b2ec7e67fbab8f29f",
 
       // License information.
       "LicenseSerial" => "1",
-      "LicenseKey" => "008fc7f4690a8e05e1215dd2fa164ce8418902517aa06d635198f7fe02ad",
+      //"LicenseKey" => "008fc7f4690a8e05e1215dd2fa164ce8418902517aa06d635198f7fe02ad",
+      "LicenseKey" => "eb8266c37e5b2f7c23104b2b37abb66bc7d2259a46f1705ba1d033305dab",
 
       // Do not change the below settings under any circumstances, or your support will be voided.
-      //"MasterServer" => "https://central.auth.core:43106", // Do not change.
-      "MasterServer" => "https://localhost:43106", // For development only!
+      "MasterServer" => "https://auth.towerdevs.xyz", // Do not change.
+      //"MasterServer" => "https://localhost:43106", // For development only!
     ];
   }
 
